c9d1319d34cf600da42433408e19e1aea79aa29a
Arun Bojja <arunsena123@gmail.com>
2019-02-18 17:43:38 -0500

    Transform property

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import TableNew from './Table.js';
+
+function transform(offset){
+  const cos = Math.cos(offset);
+  const sin = Math.sin(offset);
+  return {transform: `matrix3d(${sin}, ${-cos}, ${sin}, 0, ${-cos}, ${sin}, 0, 0, 0, ${cos}, ${cos}, ${sin}, 0, 0, 0, 1)`};
+}
 
 class App extends Component {
+  state = {
+    styleOne:{},
+    styleTwo:{}
+  }
+  onMouseMove = (event) =>{
+    this.setState({
+      styleOne: transform(-event.clientX / event.clientY),
+      styleTwo: transform(event.clientX / event.clientY),
+    });
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1> Hello React!</h1>
+        <header>
+          <h1> React!</h1>
           <img src={logo} className="App-logo" alt="logo" />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <p>Learn React</p>
-          </a>
         </header>
-        <section>
-          <TableNew />
-        </section>
+        <div onMouseMove={this.onMouseMove}>
+          <div className="panel" style={this.state.styleOne}/>
+          <div className="panel" style={this.state.styleTwo}/>
+        </div>
       </div>
     );
   }
